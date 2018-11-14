d6d3888399dc416774284b37a2e0126fa87632d6
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-14 14:16:11 +0100

    init progetto con scritta la logica a parole nel main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//creo un array contenente una lista di email
+
+//devo chiedere all'utente una sua email
+
+//controllo se effetivamente è un email
+
+//navigo la lista
+
+//eseguo il controllo se l'email esiste
+
+//se l'email esiste login effettuato in caso negativo
+//messaggio di errore
