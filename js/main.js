f08964b9ef555de3e6a2d976a759dee0f9680fb9
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-14 15:57:43 +0100

    controllo mail effettuato e settata variabile di controllo login

--- js/main.js
+++ js/main.js
@@ -6,23 +6,26 @@ console.log('Elemento array listaEmail: ' + listaEmail[1]);
 var emailUtente = prompt('Per effetuare il login mi indichi la sua email');
 //testo se abbiamo recuperato il valore
 console.log('Valore email utente: ' + emailUtente);
-//controllo se effetivamente è un email
-
+//inizializzo una variabile di controllo login
+var login;
 //navigo la lista
 for(var i=0; i < listaEmail.length; i++) {
-  //eseguo il controllo se l'email esiste
-  if (listaEmail[i].indexOf(emailUtente) != -1) {
-    console.log("email  combacia");
-    console.log("email utente " + emailUtente +" email array " + listaEmail[i]);
-    //se l'email esiste login effettuato in caso positivo
-    document.writeln("Login effettuato")
+//eseguo il confronto se esiste la mail nella lista
+  if (emailUtente == listaEmail[i]) {
+    console.log("login ok");
+    //setto la variabile con un booleano true
+    login = true;
   }
-  else {
-    console.log("email non combacia");
-    console.log("email utente " + emailUtente +" email array " + listaEmail[i]);
-    document.writeln("Login non effettuato")
-  }
+/*  else { sovvrascrive
+    console.log("no");
+    login = false;
+  } */
 }
-
-//se l'email esiste login effettuato in caso negativo
+//se la variabile è settata true allora il login è effettuato se no
 //messaggio di errore
+if (login == true) {
+  document.writeln("Login effettuato")
+}
+else {
+  document.writeln("Login non effettuato")
+}
