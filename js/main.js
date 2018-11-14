7bfff3b0cb2291d9a97a0fb8c46eeab5c102b1ba
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-14 14:19:23 +0100

    creato array di mail e testato

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 //creo un array contenente una lista di email
-
+var listaEmail = ['alonso@gmail.com','verstappen@redbull.com', 'ferrari@ferrari.it'];
+//testo se l'array effetivamente esiste
+console.log(listaEmail[1]);
 //devo chiedere all'utente una sua email
 
 //controllo se effetivamente è un email
