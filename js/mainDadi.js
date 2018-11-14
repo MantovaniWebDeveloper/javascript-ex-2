0b0ff1a2cfb25b3a6e0f3029f77e3b1384e7d824
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-14 17:11:49 +0100

    dadi finito

--- js/mainDadi.js
+++ js/mainDadi.js
@@ -4,7 +4,6 @@ var nomeUtente2 = prompt("Inserisca il suo nome giocatore 2")
 //Stampo in html i nomi
 document.getElementById('nomeGiocatore').innerHTML = nomeUtente1;
 document.getElementById('nomeGiocatore2').innerHTML = nomeUtente2;
-
 //controllo i valori dei nomi
 console.log("I giocatori sono: " + nomeUtente1 + ", " + nomeUtente2);
 //gestisco il primo concorrente che lancia il dado
@@ -13,7 +12,6 @@ var lancioConcorrente1 = Math.floor(Math.random()*7);
 //controllo il risultato
 console.log(lancioConcorrente1);
 document.getElementById('valoreLancio').innerHTML = lancioConcorrente1;
-
 //gestisco il primo concorrente che lancia il dado
 // il range deve essere da 1 a 6
 var lancioConcorrente2 = Math.floor(Math.random()*7);
@@ -27,6 +25,5 @@ if (lancioConcorrente1 > lancioConcorrente2) {
 }
 else {
   console.log("hai vinto, " + nomeUtente2);
-  document.getElementById('score2').innerHTML = "HAI PERSO";
-
+  document.getElementById('score2').innerHTML = "HAI VINTO";
 }
