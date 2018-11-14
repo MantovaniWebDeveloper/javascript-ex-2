8c156f30d3f6f0f48cd7bd999453067fb92d2c7f
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-14 16:30:45 +0100

    randomizzati i lanci dei due concorrenti

---
+++ js/mainDadi.js
@@ -0,0 +1,12 @@
+//gestisco il primo concorrente che lancia il dado
+// il range deve essere da 1 a 6
+var lancioConcorrente1 = Math.floor(Math.random()*7);
+//controllo il risultato
+console.log(lancioConcorrente1);
+
+//gestisco il primo concorrente che lancia il dado
+// il range deve essere da 1 a 6
+var lancioConcorrente2 = Math.floor(Math.random()*7);
+
+//controllo il risultato
+console.log(lancioConcorrente2);
